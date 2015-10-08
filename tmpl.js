55e65cf51c899a401acfd037714c5c7a861e8f8f
dongzhi <sundongzhi11014111@126.com>
2015-10-08 14:35:41 +0800

    update tmpl.js

--- tmpl.js
+++ tmpl.js
@@ -8,3 +8,154 @@ http://shop112763410.m.taobao.com/#list=
 2，了解使用Sequelize
 3，使用RabbitMQ 消息代理 暂时用途不明确， 例子不清晰，暂时略过。
 4，在express和socket.io之间共享session数据，实现登录退出
+
+
+
+172.16.0.254  4300   mysql  root gozapdev
+
+172.16.3.128          6390  redis
+
+172.16.1.147          9200   ES
+
+
+docker
+
+
+172.16.0.14
+
+172.16.0.17
+
+git.gozap.com
+
+172.16.1.143
+
+git@172.16.1.143:beyond/longdai-jieqianhua.git
+
+
+172.16.11.203   dohko.www.longdai.com   root 123456
+
+172.16.12.93   dohko.api.longdai.com
+
+vi dohko.www.longdai.com.conf
+
+vi /home/nginx/conf/nginx.conf
+
+ps aux | grep nginx
+
+cd vhost/longdai_com/
+
+cd /home/nginx
+
+more dohko.www.longdai.com.conf
+
+cd vhost/
+
+
+ shift+ ]
+
+ curl http://172.16.0.17:8000/logs
+
+ /home/nginx/sbin/nginx -s reload
+
+ /home/nginx/sbin/nginx -t
+
+ vi repo.gozap.com.conf
+
+ more yum.gozap.com.conf
+
+ vi repo.gozap.com.conf
+
+ tail -f api.anyview.com.log
+
+ mv dohko.api.ezhe.com.conf api.anyview.com.conf
+
+ cp dohko.api.ezhe.com.conf /home/nginx/conf/vhost/anyview_com/
+
+ ps -ef|grep logstash
+
+ tail -f /usr/local/logstash/nohup.out
+
+ sh /etc/init.d/logstashd
+
+ kill -9 16865
+
+tail -f dohko.service.jieqianhua.com.log
+
+ cat /etc/init.d/logstashd
+
+ dohko.service.jieqianhua.com.conf
+
+ /home/nginx/sbin/nginx -s reload
+
+ ping dohko.service.jieqianhua.com
+
+  ssh -l root 118.186.235.50
+
+  ssh -l root 118.186.245.50
+
+  ssh -l root 118.186.245.60
+
+  ssh -l root 58.68.151.28
+
+  ssh -l root 118.186.245.50
+
+  ssh -l root 172.16.2.32
+
+  ssh -l root 172.16.2.43
+
+  ssh -l root 192.168.2.32
+
+  ssh -l root 172.16.0.112
+
+  scp bitnami-owncloud-8.1.1-0-linux-x64-installer.run root@172.16.2.32:/home/install/
+
+  vi /root/.ssh/known_hosts
+
+  du -sh /home/install/bitnami-owncloud-7.0.4-1-ubuntu-14.04
+
+  wget -b http://downloads.bitnami.com/files/download/owncloud/bitnami-owncloud-7.0.4-1-ubuntu-14.04.zip
+
+
+
+
+
+http://localhost:8080/index.do?source=bangwoya&sn=b3f8359f2a1131b0280ec459f9a769d2
+
+http://dohko.www.longdai.com/index.do?type=register&randKey=685A575755525D06000105
+
+
+http://localhost:8080/tianmangyun/report.do?startday=20150918&endday=20150923&sign=ae4194f41fcdbed692746e7d0e4c2044
+
+http://localhost:8080/tianmangyun/bangwoyacps.do?startday=20150918&endday=20150923&sign=ae4194f41fcdbed692746e7d0e4c2044
+
+
+http://localhost:8080/payEasySyncAccountNotify.do
+
+舒依玉 430211197608058844
+
+
+
+
+
+
+
+
+
+
+
+ssh sundongzhi@223.202.52.182
+
+/home/nginx/conf/vhost/longdai_com
+
+只能从这里跳转
+
+
+数据库
+ssh root@192.168.2.105
+
+root@192.168.2.105
+
+dg2_15@br
+
+mysql -h 192.168.2.114 -umyviewol -p -P3330 -Dsp2p
+
